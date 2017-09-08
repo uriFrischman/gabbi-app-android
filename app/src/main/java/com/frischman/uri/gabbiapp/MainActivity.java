f4520bb6b223a8e4768c54884fa1c2369c56c4ea
Uri <uri.frischman@gmail.com>
2017-09-08 13:55:36 -0400

    temporary functionality to fetch all events, currently in mainactivity, will need to move to its own util, or seperate method

--- app/src/main/java/com/frischman/uri/gabbiapp/MainActivity.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/MainActivity.java
@@ -2,6 +2,17 @@ package com.frischman.uri.gabbiapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.frischman.uri.gabbiapp.GabbiApp.getAppAmazonDynamoDBClient;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +21,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Runnable x = new Runnable() {
+            @Override
+            public void run() {
+                fetchEvents();
+
+            }
+        };
+
+        Thread myThread = new Thread(x);
+
+        myThread.start();
     }
+
+
+    private ArrayList<String> fetchEvents() {
+        ArrayList<String> events = new ArrayList<>();
+
+        ScanResult result = null;
+
+        do {
+            ScanRequest req = new ScanRequest();
+            req.setTableName("Events");
+
+            if (result != null) {
+                req.setExclusiveStartKey(result.getLastEvaluatedKey());
+            }
+
+            result = getAppAmazonDynamoDBClient().scan(req);
+
+            List<Map<String, AttributeValue>> rows = result.getItems();
+
+            for (Map<String, AttributeValue> map : rows) {
+                try {
+                    AttributeValue v = map.get("event_name");
+                    String eventName = v.getS();
+                    Log.d("main", eventName);
+                    events.add(eventName);
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        } while (result.getLastEvaluatedKey() != null);
+
+        return events;
+    }
+
 }
