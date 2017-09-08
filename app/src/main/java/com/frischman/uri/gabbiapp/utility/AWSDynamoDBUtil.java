93eecbcb0b5044fb020ce6836a9e841a9797715b
Uri <uri.frischman@gmail.com>
2017-09-07 21:33:32 -0400

    removed getAppStringUtil() and replaced with direct use of its static methods

--- app/src/main/java/com/frischman/uri/gabbiapp/utility/AWSDynamoDBUtil.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/utility/AWSDynamoDBUtil.java
@@ -9,7 +9,7 @@ import com.frischman.uri.gabbiapp.R;
 import com.frischman.uri.gabbiapp.User;
 
 import static com.frischman.uri.gabbiapp.GabbiApp.getAppDynamoDBMapper;
-import static com.frischman.uri.gabbiapp.GabbiApp.getAppStringUtil;
+import static com.frischman.uri.gabbiapp.utility.StringUtil.getString;
 
 public class AWSDynamoDBUtil {
     private static final String TAG = "AWSDynamoDBUtil";
@@ -34,7 +34,7 @@ public class AWSDynamoDBUtil {
         try {
             getAppDynamoDBMapper().delete(user);
         } catch (Exception e) {
-            Log.d(TAG, getAppStringUtil().getString(R.string.exception_user_does_not_exists));
+            Log.d(TAG, getString(R.string.exception_user_does_not_exists));
         }
     }
 
@@ -43,7 +43,7 @@ public class AWSDynamoDBUtil {
             User userToDelete = getAppDynamoDBMapper().load(User.class, id);
             getAppDynamoDBMapper().delete(userToDelete);
         } catch (Exception e) {
-            Log.d(TAG, getAppStringUtil().getString(R.string.exception_user_does_not_exists));
+            Log.d(TAG, getString(R.string.exception_user_does_not_exists));
         }
     }
 }
