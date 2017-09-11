0617ef5547e5979c86e1fd1316c2a94d6f88486c
Uri Frischman <uri.frischman@gmail.com>
2017-09-11 11:37:36 -0400

    with ref to docs, staying away from default and using US locale

--- app/src/main/java/com/frischman/uri/gabbiapp/utility/DateUtil.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/utility/DateUtil.java
@@ -10,7 +10,7 @@ import java.util.Locale;
 public class DateUtil {
 
     public static Date getDateFromString(String dateString, String dateFormat) {
-        DateFormat format = new SimpleDateFormat(dateFormat, Locale.getDefault());
+        DateFormat format = new SimpleDateFormat(dateFormat, Locale.US);
         Date date = null;
         try {
             date = format.parse(dateString);
