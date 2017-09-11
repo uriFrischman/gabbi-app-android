c79fef57621ec3c981f2796f9ada2a28faff7f75
Uri Frischman <uri.frischman@gmail.com>
2017-09-11 08:01:22 -0400

    added eventDB keys to strings and replaced hard coded values

--- app/src/main/java/com/frischman/uri/gabbiapp/utility/EventUtil.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/utility/EventUtil.java
@@ -4,6 +4,7 @@ package com.frischman.uri.gabbiapp.utility;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.frischman.uri.gabbiapp.R;
 import com.frischman.uri.gabbiapp.model.Event;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 import static com.frischman.uri.gabbiapp.GabbiApp.getAppAmazonDynamoDBClient;
+import static com.frischman.uri.gabbiapp.utility.StringUtil.getString;
 
 public class EventUtil {
 
@@ -23,10 +25,10 @@ public class EventUtil {
 
         ScanResult scanResult = getAppAmazonDynamoDBClient().scan(scanRequest);
         for (Map<String, AttributeValue> item: scanResult.getItems()) {
-            String eventName = String.valueOf(item.get("event_name").getS());
-            String eventDate = String.valueOf(item.get("event_date").getS());
-            int numAliyahs = Integer.valueOf(item.get("num_aliyahs").getN());
-            int numAliyahsTaken= Integer.valueOf(item.get("num_aliyahs_taken").getN());
+            String eventName = String.valueOf(item.get(getString(R.string.key_event_name)).getS());
+            String eventDate = String.valueOf(item.get(getString(R.string.key_event_date)).getS());
+            int numAliyahs = Integer.valueOf(item.get(getString(R.string.key_num_aliyahs)).getN());
+            int numAliyahsTaken= Integer.valueOf(item.get(getString(R.string.key_num_aliyahs_taken)).getN());
             Event currentEvent = new Event(eventName, eventDate, numAliyahs, numAliyahsTaken);
             allEvents.add(currentEvent);
         }
