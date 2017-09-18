0614f748d76217cb50a042fda818a24f0528a42f
Uri Frischman <uri.frischman@gmail.com>
2017-09-18 16:48:00 -0400

    added recycler view dependency, added temporary list of all events with o’clock listener in main activity

---
+++ app/src/main/java/com/frischman/uri/gabbiapp/EventRecyclerViewAdapter.java
@@ -0,0 +1,83 @@
+package com.frischman.uri.gabbiapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.frischman.uri.gabbiapp.model.Event;
+
+import java.util.List;
+
+
+public class EventRecyclerViewAdapter extends RecyclerView.Adapter<EventRecyclerViewAdapter.ViewHolder> {
+
+    private List<Event> mEventList;
+    private LayoutInflater mLayoutInflater;
+    private ItemClickListener mItemClickListener;
+
+    public EventRecyclerViewAdapter(Context context, List<Event> eventList) {
+        mLayoutInflater = LayoutInflater.from(context);
+        mEventList = eventList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mLayoutInflater.inflate(R.layout.event_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String eventName = mEventList.get(position).getEventName();
+        holder.mTextView.setText(eventName);
+
+    }
+
+    public Event getItem(int position) {
+        return mEventList.get(position);
+    }
+
+    public void addListOfEvents(List<Event> listOfEvents){
+        mEventList = listOfEvents;
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mEventList != null) {
+            return mEventList.size();
+        } else {
+            return 0;
+        }
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView mTextView;
+
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.eventName);
+            mTextView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            mItemClickListener.onItemClick(v, getAdapterPosition());
+        }
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        mItemClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
