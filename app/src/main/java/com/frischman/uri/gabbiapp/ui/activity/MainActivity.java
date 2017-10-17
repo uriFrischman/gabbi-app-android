1db2cc4c7a4666033a3a1a4e9de160eec57d2348
Uri <uri.frischman@gmail.com>
2017-10-17 00:32:30 -0400

    formatting fix

--- app/src/main/java/com/frischman/uri/gabbiapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/ui/activity/MainActivity.java
@@ -14,6 +14,7 @@ import static com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mBinding;
+
     @Override
     public void onBackPressed() {
         if (mBinding.slidingLayout.getPanelState() == PanelState.EXPANDED) {
