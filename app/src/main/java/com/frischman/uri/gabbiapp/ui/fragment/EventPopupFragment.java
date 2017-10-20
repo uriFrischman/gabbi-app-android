19b395bd5368cd6b9ce6bd12fa34b592c0f3cd00
Uri <uri.frischman@gmail.com>
2017-10-20 03:13:07 -0400

    aliyah row item on click (for now it is just claiming aliyah if not currently taken)

--- app/src/main/java/com/frischman/uri/gabbiapp/ui/fragment/EventPopupFragment.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/ui/fragment/EventPopupFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +17,16 @@ import com.frischman.uri.gabbiapp.R;
 import com.frischman.uri.gabbiapp.databinding.FragmentEventPopupBinding;
 import com.frischman.uri.gabbiapp.loader.AliyahLoader;
 import com.frischman.uri.gabbiapp.model.Aliyah;
+import com.frischman.uri.gabbiapp.model.User;
+import com.frischman.uri.gabbiapp.ui.RecyclerViewItemClick;
 import com.frischman.uri.gabbiapp.ui.adapter.EventPopUpRecyclerViewAdapter;
 
 import java.util.List;
 import java.util.Random;
 
 import static com.frischman.uri.gabbiapp.ui.activity.MainActivity.getActivityMainBinding;
+import static com.frischman.uri.gabbiapp.utility.AliyahUtil.claimAliyah;
+import static com.frischman.uri.gabbiapp.utility.AliyahUtil.isAliyahTaken;
 import static com.frischman.uri.gabbiapp.utility.FragmentUtil.removeFragmentFromView;
 
 public class EventPopupFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Aliyah>> {
@@ -64,6 +69,18 @@ public class EventPopupFragment extends Fragment implements LoaderManager.Loader
         mBinding.eventAliyahList.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
         mBinding.eventAliyahList.setAdapter(mEventPopUpRecyclerViewAdapter);
         mBinding.eventAliyahList.addItemDecoration(new DividerItemDecoration(getActivity().getApplicationContext(), DividerItemDecoration.VERTICAL));
+        mEventPopUpRecyclerViewAdapter.setItemClickListener(new RecyclerViewItemClick() {
+            @Override
+            public void onClick(View v, int position) {
+                Aliyah aliyah = mEventPopUpRecyclerViewAdapter.getAliyah(position);
+                if (!isAliyahTaken(aliyah)) {
+                    User user = new User(1, "Uri", "Frischman", true);
+                    claimAliyah(user, aliyah);
+                } else {
+                    Log.d(TAG, "onClick: The aliyah is already taken");
+                }
+            }
+        });
     }
 
     @Override
