b283d139b5db73032d510471b753b1624cc0b041
Uri Frischman <uri.frischman@gmail.com>
2017-11-02 03:10:45 -0400

    added interface for lambda functions

---
+++ app/src/main/java/com/frischman/uri/gabbiapp/LambdaFunctions.java
@@ -0,0 +1,12 @@
+package com.frischman.uri.gabbiapp;
+
+import com.amazonaws.mobileconnectors.lambdainvoker.LambdaFunction;
+
+public interface LambdaFunctions {
+
+    @LambdaFunction(functionName = "user_login")
+    UserLoginResponse userLogin(UserLoginRequest input);
+
+    @LambdaFunction(functionName = "user_signup")
+    UserSignUpResonse userSignUp(UserSignUpRequest input);
+}
