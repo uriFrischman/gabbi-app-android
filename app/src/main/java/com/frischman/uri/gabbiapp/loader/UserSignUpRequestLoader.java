c5193835efa4500a9c9cf53fcd183a42097bf881
Uri Frischman <uri.frischman@gmail.com>
2017-11-03 16:53:47 -0400

    created User sign up request loader

---
+++ app/src/main/java/com/frischman/uri/gabbiapp/loader/UserSignUpRequestLoader.java
@@ -0,0 +1,31 @@
+package com.frischman.uri.gabbiapp.loader;
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+
+import com.amazonaws.mobileconnectors.lambdainvoker.LambdaInvokerFactory;
+import com.frischman.uri.gabbiapp.LambdaFunctions;
+import com.frischman.uri.gabbiapp.UserSignUpRequest;
+import com.frischman.uri.gabbiapp.UserSignUpResonse;
+import com.frischman.uri.gabbiapp.model.User;
+
+import static com.frischman.uri.gabbiapp.GabbiApp.getAppCognitoCachingCredentialsProvider;
+import static com.frischman.uri.gabbiapp.GabbiApp.getAppContext;
+
+public class UserSignUpRequestLoader extends AsyncTaskLoader<UserSignUpResonse> {
+
+    private User user;
+
+    public UserSignUpRequestLoader(Context context, User user) {
+        super(context);
+        this.user = user;
+    }
+
+    @Override
+    public UserSignUpResonse loadInBackground() {
+        LambdaInvokerFactory factory = LambdaInvokerFactory.builder().context(getAppContext()).credentialsProvider(getAppCognitoCachingCredentialsProvider()).build();
+        final LambdaFunctions lambdaFunctions = factory.build(LambdaFunctions.class);
+        final UserSignUpRequest userSignUpRequest = new UserSignUpRequest(user);
+        return lambdaFunctions.userSignUp(userSignUpRequest);
+    }
+}
