b42b1d079e25395da83e4172608ddee72ba4846d
Uri Frischman <uri.frischman@gmail.com>
2017-11-03 14:54:27 -0400

    initial signup request and response models

---
+++ app/src/main/java/com/frischman/uri/gabbiapp/UserSignUpRequest.java
@@ -0,0 +1,83 @@
+package com.frischman.uri.gabbiapp;
+
+import java.util.Random;
+
+public class UserSignUpRequest {
+
+    int userId;
+    String userName;
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
+    String password;
+
+    public UserSignUpRequest() {
+    }
+
+    public UserSignUpRequest(String userName, String firstName, String lastName, String email, String phoneNumber, String password) {
+        this.userId = new Random().nextInt(1000);
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/frischman/uri/gabbiapp/UserSignUpResonse.java
@@ -0,0 +1,33 @@
+package com.frischman.uri.gabbiapp;
+
+import com.frischman.uri.gabbiapp.model.User;
+
+public class UserSignUpResonse {
+
+    User user;
+    boolean succesfulSignUp;
+
+    public UserSignUpResonse() {
+    }
+
+    public UserSignUpResonse(User user, boolean succesfulSignUp) {
+        this.user = user;
+        this.succesfulSignUp = succesfulSignUp;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isSuccesfulSignUp() {
+        return succesfulSignUp;
+    }
+
+    public void setSuccesfulSignUp(boolean succesfulSignUp) {
+        this.succesfulSignUp = succesfulSignUp;
+    }
+}
