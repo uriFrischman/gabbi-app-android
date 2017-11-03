9b9cdc74ff4230d8f626143b5b028a02c5335b97
Uri Frischman <uri.frischman@gmail.com>
2017-11-03 16:43:41 -0400

    ignoring login lambda function until further notice

--- app/src/main/java/com/frischman/uri/gabbiapp/LambdaFunctions.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/LambdaFunctions.java
@@ -4,8 +4,8 @@ import com.amazonaws.mobileconnectors.lambdainvoker.LambdaFunction;
 
 public interface LambdaFunctions {
 
-    @LambdaFunction(functionName = "user_login")
-    UserLoginResponse userLogin(UserLoginRequest input);
+//    @LambdaFunction(functionName = "user_login")
+//    UserLoginResponse userLogin(UserLoginRequest input);
 
     @LambdaFunction(functionName = "user_signup")
     UserSignUpResonse userSignUp(UserSignUpRequest input);
