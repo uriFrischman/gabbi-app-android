0d3207aef0a7eee74cc1005d9189d26b247758ca
Uri Frischman <uri.frischman@gmail.com>
2017-11-07 23:44:17 -0500

    removed unused method and unused variables

--- app/src/main/java/com/frischman/uri/gabbiapp/ui/fragment/SignUpFragment.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/ui/fragment/SignUpFragment.java
@@ -4,7 +4,6 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +14,6 @@ import com.frischman.uri.gabbiapp.databinding.FragmentSignupBinding;
 
 public class SignUpFragment extends Fragment {
 
-    private static final String TAG = "SignUpFragment";
     private FragmentSignupBinding mBinding;
 
     @Nullable
@@ -27,22 +25,11 @@ public class SignUpFragment extends Fragment {
         mBinding.signupFragmentSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: Button clicked!");
             }
         });
 
         return mBinding.getRoot();
     }
 
-    private Bundle gatherInputsToBundle() {
-        Bundle args = new Bundle();
-        args.putString("firstName", mBinding.signupFragmentFirstname.getText().toString());
-        args.putString("lastName", mBinding.signupFragmentLastname.getText().toString());
-        args.putString("username", mBinding.signupFragmentUsername.getText().toString());
-        args.putString("password", mBinding.signupFragmentPassword.getText().toString());
-        args.putString("email", mBinding.signupFragmentEmail.getText().toString());
-        args.putString("phoneNumber", mBinding.signupFragmentPhoneNumber.getText().toString());
-        args.putBoolean("isGabbi", mBinding.signupFragmentIsGabbi.isChecked());
-        return args;
     }
 }
