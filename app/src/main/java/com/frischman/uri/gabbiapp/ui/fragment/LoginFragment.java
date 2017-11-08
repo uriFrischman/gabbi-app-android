8f34e4cf6eafd7b101e6400c78cec5a555820acb
Uri <uri.frischman@gmail.com>
2017-11-08 00:42:45 -0500

    implement user login loader and its callbacks

--- app/src/main/java/com/frischman/uri/gabbiapp/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/ui/fragment/LoginFragment.java
@@ -1,26 +1,47 @@
 package com.frischman.uri.gabbiapp.ui.fragment;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.frischman.uri.gabbiapp.R;
 import com.frischman.uri.gabbiapp.databinding.FragmentLoginBinding;
+import com.frischman.uri.gabbiapp.loader.UserLoginRequestLoader;
+import com.frischman.uri.gabbiapp.network.response.UserLoginResponse;
+import com.frischman.uri.gabbiapp.ui.activity.MainActivity;
 
+import static android.content.ContentValues.TAG;
 import static com.frischman.uri.gabbiapp.utility.FragmentUtil.replaceViewWithFragment;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements LoaderManager.LoaderCallbacks<UserLoginResponse> {
 
     private FragmentLoginBinding mBinding;
+    private LoginFragment mContext = this;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+
+        mBinding.loginFragmentLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked!");
+                Bundle args = new Bundle();
+                args.putString("username", mBinding.loginUsernameField.getText().toString());
+                args.putString("password", mBinding.loginPasswordField.getText().toString());
+
+                getActivity().getSupportLoaderManager().restartLoader(1, args, mContext).forceLoad();
+            }
+        });
 
         mBinding.loginFragmentSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +51,27 @@ public class LoginFragment extends Fragment {
         });
 
         return mBinding.getRoot();
+    }
+
+    @Override
+    public Loader<UserLoginResponse> onCreateLoader(int id, Bundle args) {
+        String username = args.getString("username");
+        String password = args.getString("password");
+        return new UserLoginRequestLoader(getActivity().getApplicationContext(), username, password);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<UserLoginResponse> loader, UserLoginResponse data) {
+        Log.d(TAG, "onLoadFinished: " + data.toString());
+        if (data.isSuccessfulLogin()) {
+            Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<UserLoginResponse> loader) {
+
     }
 }
