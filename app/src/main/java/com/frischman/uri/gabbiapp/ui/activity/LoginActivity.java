bcfcb92540b386fcebfd7a6b36064e9aee254bce
Uri Frischman <uri.frischman@gmail.com>
2017-11-07 23:45:44 -0500

    check if user is logged in when app launches

--- app/src/main/java/com/frischman/uri/gabbiapp/ui/activity/LoginActivity.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/ui/activity/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.frischman.uri.gabbiapp.ui.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.frischman.uri.gabbiapp.R;
 import com.frischman.uri.gabbiapp.databinding.ActivityLoginBinding;
@@ -20,8 +23,21 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkIfUserIsLoggedIn();
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         replaceViewWithFragment(getSupportFragmentManager(), R.id.login_activity_container, new LoginFragment(), false);
+    }
+
+    private void checkIfUserIsLoggedIn() {
+        SharedPreferences sharedPreferences = getSharedPreferences("UserInfo", 0);
+        if (sharedPreferences.contains("userInfo")) {
+            Log.d(TAG, "onCreate: User is in shared preferences");
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            this.finish();
+        } else {
+            Log.d(TAG, "onCreate: User is not in shared preferences");
+        }
     }
 
 }
