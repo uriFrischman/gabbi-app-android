3e40ee6eb244a7b3945fde7ba4c6f65ade280fd7
Uri <uri.frischman@gmail.com>
2017-12-26 08:59:19 -0500

    integer range arraylist function created

--- app/src/main/java/com/frischman/uri/gabbiapp/utility/IntegerUtil.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/utility/IntegerUtil.java
@@ -1,9 +1,19 @@
 package com.frischman.uri.gabbiapp.utility;
 
 
+import java.util.ArrayList;
+
 public class IntegerUtil {
 
     public boolean intToBoolean(int integer) {
         return integer != 0;
+    }
+
+    public static ArrayList<Integer> getRange(int begin, int end) {
+        ArrayList<Integer> range = new ArrayList<>();
+        for (int i = begin; i <= end; i++) {
+            range.add(i);
+        }
+        return range;
     }
 }
