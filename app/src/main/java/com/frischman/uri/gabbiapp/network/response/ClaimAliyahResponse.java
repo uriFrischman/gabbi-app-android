a568804cec09bafbd9d48022151d969e12fc7d68
Uri <uri.frischman@gmail.com>
2018-01-03 13:55:09 -0500

    created fields for claim aliyah response

--- app/src/main/java/com/frischman/uri/gabbiapp/network/response/ClaimAliyahResponse.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/network/response/ClaimAliyahResponse.java
@@ -1,6 +1,42 @@
 package com.frischman.uri.gabbiapp.network.response;
 
 
+import com.frischman.uri.gabbiapp.model.Aliyah;
+
 public class ClaimAliyahResponse {
     
+
+    Aliyah aliyah;
+    String message;
+    boolean succesfullClaim;
+
+    public ClaimAliyahResponse(Aliyah aliyah, String message, boolean succesfullClaim) {
+        this.aliyah = aliyah;
+        this.message = message;
+        this.succesfullClaim = succesfullClaim;
+    }
+
+    public Aliyah getAliyah() {
+        return aliyah;
+    }
+
+    public void setAliyah(Aliyah aliyah) {
+        this.aliyah = aliyah;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccesfullClaim() {
+        return succesfullClaim;
+    }
+
+    public void setSuccesfullClaim(boolean succesfullClaim) {
+        this.succesfullClaim = succesfullClaim;
+    }
 }
