427eff94285f7f00d32746b7ecd77406272aba6a
Uri <uri.frischman@gmail.com>
2018-01-02 22:34:09 -0500

    added parameters to claim aliyah request and created its loader

--- app/src/main/java/com/frischman/uri/gabbiapp/network/request/ClaimAliyahRequest.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/network/request/ClaimAliyahRequest.java
@@ -1,6 +1,32 @@
 package com.frischman.uri.gabbiapp.network.request;
 
 
+import com.frischman.uri.gabbiapp.model.Aliyah;
+import com.frischman.uri.gabbiapp.model.User;
+
 public class ClaimAliyahRequest {
 
+    private Aliyah aliyah;
+    private User user;
+
+    public ClaimAliyahRequest(Aliyah aliyah, User user) {
+        this.aliyah = aliyah;
+        this.user = user;
+    }
+
+    public Aliyah getAliyah() {
+        return aliyah;
+    }
+
+    public void setAliyah(Aliyah aliyah) {
+        this.aliyah = aliyah;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
