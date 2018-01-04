ae019c238f1319b26fdd61d05b10efae8011dd1d
Uri <uri.frischman@gmail.com>
2018-01-04 11:03:53 -0500

    added parameters to request/response and created get events loader

--- app/src/main/java/com/frischman/uri/gabbiapp/network/request/GetEventsRequest.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/network/request/GetEventsRequest.java
@@ -2,4 +2,18 @@ package com.frischman.uri.gabbiapp.network.request;
 
 
 public class GetEventsRequest {
+
+    boolean containPastEvents;
+
+    public GetEventsRequest(boolean containPastEvents) {
+        this.containPastEvents = containPastEvents;
+    }
+
+    public boolean isContainPastEvents() {
+        return containPastEvents;
+    }
+
+    public void setContainPastEvents(boolean containPastEvents) {
+        this.containPastEvents = containPastEvents;
+    }
 }
--- app/src/main/java/com/frischman/uri/gabbiapp/network/response/GetEventsResponse.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/network/response/GetEventsResponse.java
@@ -1,5 +1,23 @@
 package com.frischman.uri.gabbiapp.network.response;
 
 
+import com.frischman.uri.gabbiapp.model.Event;
+
+import java.util.List;
+
 public class GetEventsResponse {
+
+    List<Event> listOfEvents;
+
+    public GetEventsResponse(List<Event> listOfEvents) {
+        this.listOfEvents = listOfEvents;
+    }
+
+    public List<Event> getListOfEvents() {
+        return listOfEvents;
+    }
+
+    public void setListOfEvents(List<Event> listOfEvents) {
+        this.listOfEvents = listOfEvents;
+    }
 }
