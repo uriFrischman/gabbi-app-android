07524ac35f904d9b8dc9e07829d35e960ed601c9
Uri <uri.frischman@gmail.com>
2018-01-04 15:01:56 -0500

    added get aliyahs lambda function and added its request and response classes

--- app/src/main/java/com/frischman/uri/gabbiapp/network/lambda/LambdaFunctions.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/network/lambda/LambdaFunctions.java
@@ -2,10 +2,12 @@ package com.frischman.uri.gabbiapp.network.lambda;
 
 import com.amazonaws.mobileconnectors.lambdainvoker.LambdaFunction;
 import com.frischman.uri.gabbiapp.network.request.ClaimAliyahRequest;
+import com.frischman.uri.gabbiapp.network.request.GetAliyahsRequest;
 import com.frischman.uri.gabbiapp.network.request.GetEventsRequest;
 import com.frischman.uri.gabbiapp.network.request.UserLoginRequest;
 import com.frischman.uri.gabbiapp.network.request.UserSignUpRequest;
 import com.frischman.uri.gabbiapp.network.response.ClaimAliyahResponse;
+import com.frischman.uri.gabbiapp.network.response.GetAliyahsResponse;
 import com.frischman.uri.gabbiapp.network.response.GetEventsResponse;
 import com.frischman.uri.gabbiapp.network.response.UserLoginResponse;
 import com.frischman.uri.gabbiapp.network.response.UserSignUpResponse;
@@ -23,4 +25,7 @@ public interface LambdaFunctions {
 
     @LambdaFunction(functionName = "get_events")
     GetEventsResponse getEvents(GetEventsRequest input);
+
+    @LambdaFunction(functionName = "get_aliyahs")
+    GetAliyahsResponse getAliyahs(GetAliyahsRequest input);
 }
