6a2bcab61f948a7f045c89345ce82bba5a16ac42
Uri <uri.frischman@gmail.com>
2018-01-05 15:23:07 -0500

    removed integerutil variable from aliyah row and its adapter

--- app/src/main/java/com/frischman/uri/gabbiapp/utility/IntegerUtil.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/utility/IntegerUtil.java
@@ -5,10 +5,6 @@ import java.util.ArrayList;
 
 public class IntegerUtil {
 
-    public boolean intToBoolean(int integer) {
-        return integer != 0;
-    }
-
     public static ArrayList<Integer> getRange(int begin, int end) {
         ArrayList<Integer> range = new ArrayList<>();
         for (int i = begin; i <= end; i++) {
