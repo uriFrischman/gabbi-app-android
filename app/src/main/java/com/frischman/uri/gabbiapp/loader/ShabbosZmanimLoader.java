9b6af7fb769c19efcdd4da9e197058b64ec8c756
Uri Frischman <uri.frischman@gmail.com>
2018-02-21 00:39:57 -0500

    added lambda function and created a loader

--- app/src/main/java/com/frischman/uri/gabbiapp/loader/ShabbosZmanimLoader.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/loader/ShabbosZmanimLoader.java
@@ -1,5 +1,25 @@
 package com.frischman.uri.gabbiapp.loader;
 
 
-public class ShabbosZmanimLoader {
+import android.support.v4.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.frischman.uri.gabbiapp.network.request.ShabbosZmanimRequest;
+import com.frischman.uri.gabbiapp.network.response.ShabbosZmanimResponse;
+
+import static com.frischman.uri.gabbiapp.utility.AWSLambdaUtil.getLambdaFunctions;
+
+public class ShabbosZmanimLoader extends AsyncTaskLoader<ShabbosZmanimResponse> {
+
+    private int geoNameId;
+
+    public ShabbosZmanimLoader(Context context, int geoNameId) {
+        super(context);
+        this.geoNameId = geoNameId;
+    }
+
+    @Override
+    public ShabbosZmanimResponse loadInBackground() {
+        return getLambdaFunctions().getShabbosZmanim(new ShabbosZmanimRequest(geoNameId));
+    }
 }
