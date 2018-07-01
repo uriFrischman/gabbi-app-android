c327f81d319120b5d2e17275cfa0353dc7e89192
Uri <uri.frischman@gmail.com>
2018-07-01 19:40:48 -0400

    updated LoginActivity

--- app/src/main/java/com/frischman/uri/gabbiapp/ui/activity/LoginActivity.java
+++ app/src/main/java/com/frischman/uri/gabbiapp/ui/activity/LoginActivity.java
@@ -2,22 +2,17 @@ package com.frischman.uri.gabbiapp.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.frischman.uri.gabbiapp.R;
-import com.frischman.uri.gabbiapp.databinding.ActivityLoginBinding;
 import com.frischman.uri.gabbiapp.ui.fragment.LoginFragment;
 
-import static com.frischman.uri.gabbiapp.utility.FragmentUtil.replaceViewWithFragment;
 import static com.frischman.uri.gabbiapp.utility.SharedPreferencesUtil.checkIfSharedPreferencesContainsKey;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends GabbiAppBaseActivity {
 
-    private ActivityLoginBinding mBinding;
     private static final String TAG = "LoginActivity";
 
 
@@ -25,8 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         checkIfUserIsLoggedIn();
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
-        replaceViewWithFragment(getSupportFragmentManager(), R.id.login_activity_container, new LoginFragment(), false);
+        addFragmentToActivity(new LoginFragment());
     }
 
     private void checkIfUserIsLoggedIn() {
